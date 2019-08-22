454fe2ed259c35fa7174f46893d3d5625364853e
origami <you@example.com>
2019-08-22 23:17:32 +0300

    added the ajax to visitors.php

--- visitors.php
+++ visitors.php
@@ -1,9 +1,7 @@
 <?php
 	$con = mysqli_connect('localhost', 'id8287912_origami1024', 'qwe123', 'id8287912_msgsbase');
-	//echo($_SERVER['REMOTE_ADDR']);
 	$sql="insert into visitors(ip) values ('" . $_SERVER['REMOTE_ADDR'] . "');";
-	
 	$result = mysqli_query($con, $sql);
-	echo($result);
 	mysqli_close($con);
+	echo('1');
 ?>
